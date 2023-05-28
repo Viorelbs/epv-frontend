7a157b2d6f7fb5b9bf322d9bc6e60eec72d4d20f
ViorelMG5 <viorel@mg5.ro>
2023-05-29 02:02:08 +0300

    added dynamic import for mobile menu

---
+++ components/Layout/MenuDrawer.tsx
@@ -0,0 +1,130 @@
+import React, { SetStateAction } from "react";
+import Image from "next/image";
+import { AiOutlineClose } from "react-icons/ai";
+import { SwipeableDrawer } from "@mui/material";
+import Link from "next/link";
+import Icon from "../Common/Icon";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import { LinkType } from "@/typings";
+import logo from "../../public/logo.png";
+import IndustrialForm from "./IndustrialForm";
+
+interface Props {
+  container: any;
+  toggleDrawer: (newOpen: boolean) => () => void;
+  handleOpen: () => void;
+  setOpen: (value: SetStateAction<boolean>) => void;
+  open: boolean;
+  openAc: boolean;
+  data: any;
+}
+
+export default function MenuDrawer({
+  container,
+  toggleDrawer,
+  handleOpen,
+  setOpen,
+  open,
+  openAc,
+  data,
+}: Props) {
+  return (
+    <SwipeableDrawer
+      container={container}
+      anchor="left"
+      open={open}
+      onClose={toggleDrawer(false)}
+      onOpen={toggleDrawer(true)}
+      disableSwipeToOpen={false}
+      ModalProps={{
+        keepMounted: true,
+      }}
+    >
+      <div className="min-w-[100vw] sm:min-w-[80vw] h-full px-10 flex flex-col justify-between">
+        <div>
+          <div className="py-4 flex justify-between items-center border-b mb-4">
+            <Image
+              src={logo}
+              alt="Epv logo"
+              className="w-[50px] h-[50px] object-contain invert"
+            />
+            <AiOutlineClose
+              onClick={toggleDrawer(false)}
+              className="w-7 h-7 cursor-pointer hover:text-red-500"
+            />
+          </div>
+          <div className="flex flex-col gap-2 text-[20px]  ">
+            <Link
+              className="border-b py-3"
+              href="/"
+              onClick={() => setOpen(false)}
+            >
+              Acasă
+            </Link>
+            <Link
+              className="border-b py-3"
+              href="/despre-noi"
+              onClick={() => setOpen(false)}
+            >
+              Despre Noi
+            </Link>
+            {/* <Link
+                className="border-b py-3"
+                href="/produse"
+                onClick={() => setOpen(false)}
+              >
+                Produse
+              </Link> */}
+
+            <Accordion open={openAc} icon={<Icon open={openAc} />}>
+              <AccordionHeader
+                onClick={handleOpen}
+                className="font-normal text-[20px] text-[#000000de] py-3 font-poppins"
+              >
+                Servicii
+              </AccordionHeader>
+              {data && (
+                <AccordionBody className="flex flex-col gap-4 text-md">
+                  {data.serviciis.data.map((link: LinkType) => (
+                    <Link
+                      className="text-base "
+                      onClick={() => setOpen(false)}
+                      href={`/servicii/${link.attributes.slug}`}
+                      key={link.attributes.slug}
+                    >
+                      {link.attributes.title}
+                    </Link>
+                  ))}
+                </AccordionBody>
+              )}
+            </Accordion>
+            <Link
+              href="/blog"
+              className="border-b py-3"
+              onClick={() => setOpen(false)}
+            >
+              {" "}
+              Blog
+            </Link>
+
+            <Link
+              className="border-b py-3"
+              href="/contact"
+              onClick={() => setOpen(false)}
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+
+        <div className="mb-[5vh]">
+          <IndustrialForm MenuOpen={(arg) => setOpen(arg)} />
+        </div>
+      </div>
+    </SwipeableDrawer>
+  );
+}
--- components/Layout/MobileMenu.tsx
+++ components/Layout/MobileMenu.tsx
@@ -1,29 +1,28 @@
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import { SwipeableDrawer } from "@mui/material";
-import logo from "../../public/logo.png";
+import { AiOutlineMenu } from "react-icons/ai";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import Icon from "../Common/Icon";
-import {
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
-} from "@material-tailwind/react";
+
 import { useQuery } from "@apollo/client";
 import { QUERY_SERVICES_MENU } from "@/queries/queries";
-import { LinkType } from "@/typings";
 import Cart from "./Cart";
 import { RiHeartsLine } from "react-icons/ri";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
-import IndustrialForm from "./IndustrialForm";
+import logo from "../../public/logo.png";
+import dynamic from "next/dynamic";
+import Loader from "../Common/Loader";
 
 interface Props {
   window?: () => Window;
   scroll: boolean;
 }
+
+const DynamicMenuDrawer = dynamic(() => import("./MenuDrawer"), {
+  loading: () => <Loader size={6} />,
+  ssr: false,
+});
 
 export default function MobileMenu(props: Props) {
   const { window, scroll } = props;
@@ -74,99 +73,15 @@ export default function MobileMenu(props: Props) {
           />
         </div>
       </div>
-      <SwipeableDrawer
+      <DynamicMenuDrawer
         container={container}
-        anchor="left"
+        toggleDrawer={toggleDrawer}
+        handleOpen={handleOpen}
+        setOpen={setOpen}
+        openAc={openAc}
+        data={data}
         open={open}
-        onClose={toggleDrawer(false)}
-        onOpen={toggleDrawer(true)}
-        disableSwipeToOpen={false}
-        ModalProps={{
-          keepMounted: true,
-        }}
-      >
-        <div className="min-w-[100vw] sm:min-w-[80vw] h-full px-10 flex flex-col justify-between">
-          <div>
-            <div className="py-4 flex justify-between items-center border-b mb-4">
-              <Image
-                src={logo}
-                alt="Epv logo"
-                className="w-[50px] h-[50px] object-contain invert"
-              />
-              <AiOutlineClose
-                onClick={toggleDrawer(false)}
-                className="w-7 h-7 cursor-pointer hover:text-red-500"
-              />
-            </div>
-            <div className="flex flex-col gap-2 text-[20px]  ">
-              <Link
-                className="border-b py-3"
-                href="/"
-                onClick={() => setOpen(false)}
-              >
-                Acasă
-              </Link>
-              <Link
-                className="border-b py-3"
-                href="/despre-noi"
-                onClick={() => setOpen(false)}
-              >
-                Despre Noi
-              </Link>
-              {/* <Link
-                className="border-b py-3"
-                href="/produse"
-                onClick={() => setOpen(false)}
-              >
-                Produse
-              </Link> */}
-
-              <Accordion open={openAc} icon={<Icon open={openAc} />}>
-                <AccordionHeader
-                  onClick={handleOpen}
-                  className="font-normal text-[20px] text-[#000000de] py-3 font-poppins"
-                >
-                  Servicii
-                </AccordionHeader>
-                {data && (
-                  <AccordionBody className="flex flex-col gap-4 text-md">
-                    {data.serviciis.data.map((link: LinkType) => (
-                      <Link
-                        className="text-base "
-                        onClick={() => setOpen(false)}
-                        href={`/servicii/${link.attributes.slug}`}
-                        key={link.attributes.slug}
-                      >
-                        {link.attributes.title}
-                      </Link>
-                    ))}
-                  </AccordionBody>
-                )}
-              </Accordion>
-              <Link
-                href="/blog"
-                className="border-b py-3"
-                onClick={() => setOpen(false)}
-              >
-                {" "}
-                Blog
-              </Link>
-
-              <Link
-                className="border-b py-3"
-                href="/contact"
-                onClick={() => setOpen(false)}
-              >
-                Contact
-              </Link>
-            </div>
-          </div>
-
-          <div className="mb-[5vh]">
-            <IndustrialForm MenuOpen={(arg) => setOpen(arg)} />
-          </div>
-        </div>
-      </SwipeableDrawer>
+      />
     </div>
   );
 }
