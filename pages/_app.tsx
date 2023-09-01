d147abdb3a902682bf923a704bd317197bb01592
ViorelMG5 <viorel@mg5.ro>
2023-09-01 10:07:36 +0300

    added g-id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -69,14 +69,14 @@ export default function App({ Component, pageProps }: AppProps) {
             <StrapiApolloProvider>
               <WidthProvider>
                 <Layout>
-                  <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
+                  <Script src="https://www.googletagmanager.com/gtag/js?id=G-BS9S0YXMLF" />
                   <Script id="google-analytics">
                     {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', 'GA_MEASUREMENT_ID');
+          gtag('config', 'G-BS9S0YXMLF');
         `}
                   </Script>
                   <Component {...pageProps} />
